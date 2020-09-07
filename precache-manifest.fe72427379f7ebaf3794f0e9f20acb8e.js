016043fe78f64ec76a4bf06a4b3f2747caf9e577
jordansu <jdsu1996@gmail.com>
2020-09-07 11:25:30 +0800

    Updates

---
+++ precache-manifest.fe72427379f7ebaf3794f0e9f20acb8e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b56b1c80fd08aabdab083c546476db2",
+    "url": "/Test3/index.html"
+  },
+  {
+    "revision": "356f4a8fc2c65291e3c1",
+    "url": "/Test3/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5a6b8a06ab0b64e390d5",
+    "url": "/Test3/static/js/2.8e14782c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Test3/static/js/2.8e14782c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "356f4a8fc2c65291e3c1",
+    "url": "/Test3/static/js/main.99375a97.chunk.js"
+  },
+  {
+    "revision": "87f39137e499b4d24ffe",
+    "url": "/Test3/static/js/runtime-main.9a6e0366.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Test3/static/media/logo.5d5d9eef.svg"
+  }
+]);
