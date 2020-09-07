a71b080ad4cb339d1ddedf1b90dee3419e272ee6
jordansu <jdsu1996@gmail.com>
2020-09-07 11:44:10 +0800

    Your awesome message

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ function App() {
          <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
             <p>
-               Edit <code>src/App.js</code> and trying to do something better.
+               Edit <code>src/App.js</code> and trying to do something better
+               than this items.
             </p>
             <a
                className="App-link"
